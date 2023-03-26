493da95ceac7a1b29ab7f437dcfd105ed3ed4982
Agu <zochaga@gmail.com>
2023-03-26 14:47:36 +0200

    fetch content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import LogginForm from './components/LogginForm/LogginForm'
@@ -9,9 +9,17 @@ import uuid from 'react-uuid'
 function App() {
   const [panes, setPanes] = useState([{ name: 'Agu', id: null }])
   const [selectedPane, setSelectedPane] = useState(null)
-  const [currentSnippet, setCurrentSnippet] = useState(null)
+  const [currentSnippetId, setCurrentSnippetId] = useState(null) //snippetId
 
-  const { name, snippets } = panes.find((pane) => pane.id === selectedPane)
+  const {
+    id: userId,
+    name,
+    snippets,
+  } = panes.find((pane) => pane.id === selectedPane)
+
+  const currentSnippet = snippets?.find(
+    (snippet) => snippet.id === currentSnippetId
+  )
 
   function addTab(newPanes, newId) {
     setPanes(newPanes)
@@ -68,7 +76,10 @@ function App() {
         method: 'GET',
         headers: { 'user-id': newUserDataResponse.id },
       })
-      snippets = snippetsResponse.snippets
+      snippets = snippetsResponse.snippets.map((snippet) => ({
+        ...snippet,
+        status: 'IDLE',
+      }))
     }
 
     const newUserData = {
@@ -91,18 +102,18 @@ function App() {
         pane.id === selectedPane ? { ...pane, snippets: [] } : pane
       )
       setPanes(newPanes)
-      setCurrentSnippet(null)
+      setCurrentSnippetId(null)
       return
     }
 
     const newSnippets = snippets.filter((item) => item.id !== chosen)
-    if (currentSnippet === chosen) {
+    if (currentSnippetId === chosen) {
       const newCurrentIndex =
         snippets.map((item) => item.id).indexOf(chosen) - 1
 
       newCurrentIndex !== -1
-        ? setCurrentSnippet(newSnippets[newCurrentIndex].id)
-        : setCurrentSnippet(newSnippets[0].id)
+        ? setCurrentSnippetId(newSnippets[newCurrentIndex].id)
+        : setCurrentSnippetId(newSnippets[0].id)
     }
 
     const newPanes = panes.map((pane) =>
@@ -115,15 +126,47 @@ function App() {
     const newId = uuid()
     const newSnippets = [
       ...snippets,
-      { id: newId, title: null, snippet: null, isNew: true },
+      { id: newId, title: null, snippet: null, isNew: true, status: 'SUCCESS' },
     ]
     const newPanes = panes.map((pane) =>
       pane.id === selectedPane ? { ...pane, snippets: newSnippets } : pane
     )
     setPanes(newPanes)
 
-    setCurrentSnippet(newId)
+    setCurrentSnippetId(newId)
   }
+
+  useEffect(() => {
+    if (!currentSnippet) return
+    const { isNew, status } = currentSnippet
+    if (isNew) return
+    if (status !== 'IDLE') return
+
+    async function fetchItems() {
+      const contentResponse = await toDoFetch(`/todo/${currentSnippetId}`, {
+        method: 'GET',
+        headers: {
+          'user-id': userId,
+        },
+      })
+
+      function addContentToSnippet(pane) {
+        const newSnippets = pane.snippets.map((snippet) =>
+          snippet.id === currentSnippetId
+            ? { ...snippet, ...contentResponse, status: 'SUCCESS' }
+            : snippet
+        )
+        return { ...pane, snippets: newSnippets }
+      }
+
+      const newPanes = panes.map((pane) =>
+        pane.id === selectedPane ? addContentToSnippet(pane) : pane
+      )
+
+      setPanes(newPanes)
+    }
+    fetchItems()
+  }, [currentSnippet, currentSnippetId, panes, selectedPane, userId])
 
   return (
     <div>
@@ -142,8 +185,8 @@ function App() {
       ) : (
         <Sider
           snippets={snippets}
-          currentSnippet={currentSnippet}
-          setCurrentSnippet={setCurrentSnippet}
+          currentSnippet={currentSnippetId}
+          setCurrentSnippet={setCurrentSnippetId}
           deleteChoosenSnippet={deleteChoosenSnippet}
           addNewSnippet={addNewSnippet}
         />
